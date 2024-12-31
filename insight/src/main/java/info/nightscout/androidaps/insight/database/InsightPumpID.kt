b59c4526d8ce6726b66672ae62a1a08b4b4c53cc
Winfried Kuiper <winni67@users.noreply.github.com>
2024-12-31 17:52:41 +0100

    Update InsightPumpID.kt
    syntax errors

--- insight/src/main/java/info/nightscout/androidaps/insight/database/InsightPumpID.kt
+++ insight/src/main/java/info/nightscout/androidaps/insight/database/InsightPumpID.kt
@@ -4,18 +4,20 @@ import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = DATABASE_INSIGHT_PUMP_IDS,
+@Entity(
+    tableName = DATABASE_INSIGHT_PUMP_IDS,
     indices = [
         Index("timestamp"),
         Index("pumpSerial"),
-        Index("eventType")
-    ])
+        Index("eventType"),
+    ],
+)
 data class InsightPumpID(
     var timestamp: Long,
     var eventType: EventType = EventType.None,
     val pumpSerial: String? = null,
     @PrimaryKey
-    var eventID: Long
+    var eventID: Long,
 ) {
     enum class EventType {
         PumpStarted,
@@ -23,6 +25,6 @@ data class InsightPumpID(
         PumpPaused,
         StartOfTBR,
         EndOfTBR,
-        None;
+        None,
     }
 }
