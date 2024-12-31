73cdd9a7e1934f9c67eee897ba998e53f3fd30d2
Winfried Kuiper <winni67@users.noreply.github.com>
2024-12-31 18:06:13 +0100

    Update InsightModule.kt
    syntax errors

--- insight/src/main/java/info/nightscout/androidaps/insight/di/InsightModule.kt
+++ insight/src/main/java/info/nightscout/androidaps/insight/di/InsightModule.kt
@@ -3,12 +3,14 @@ package info.nightscout.androidaps.insight.di
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
-@Module(includes = [
+@Module(
+    includes = [
     InsightCommModule::class,
     InsightActivitiesModule::class,
     InsightServicesModule::class,
     InsightDatabaseModule::class
-])
+],
+)
 
 @Suppress("unused")
 abstract class InsightModule
