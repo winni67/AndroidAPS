25e6f3d6356535e3a929eef47f1ff478a0430d3c
Winfried Kuiper <winni67@users.noreply.github.com>
2026-01-03 19:50:58 +0100

    Update Versions.kt
    Activate autoISF in master branch

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 @Suppress("ConstPropertyName")
 object Versions {
 
-    const val appVersion = "3.4.0.0"
+    const val appVersion = "3.4.0.0-aisf3.1.x"
     const val versionCode = 1500
 
     const val compileSdk = 36
